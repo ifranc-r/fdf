1e792b89cfd448384822fb16f6188b22e4819b9b
Andy RAMANANARIVO <aramanan@e1r10p23.42.fr>
2016-02-19 18:24:11 +0100

    deplacement init_tab_coord et factorisation

--- srcs/coord/fill_coord.c
+++ srcs/coord/fill_coord.c
@@ -6,39 +6,44 @@
 /*   By: aramanan <aramanan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/19 13:22:09 by aramanan          #+#    #+#             */
-/*   Updated: 2016/02/19 13:55:42 by aramanan         ###   ########.fr       */
+/*   Updated: 2016/02/19 18:23:12 by aramanan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/fdf.h"
 
-// TODO: creer fonction qui rempli ligne par ligne tab_coord
-
-void		fill_line_tab_coord(char **map, t_coord **coord)
+static		nbline(char ***map)
 {
-	int		i;
-	int		j;
+	int		nline;
 
-	i = 0;
-	j = 0;
-	if (map[i][j])
-	{
-		coord[i][j].x = 440;
-		coord[i][j].z = 50;
-		coord[i][j].y = coord[0][0].z + coord[0][0].x;
-	}
-	j++;
+	nline = 0;
 	while (*map)
 	{
-		coord[i][j].x = 50 + coord[i][j - 1].x;
-		coord[i][j].z = 40 + coord[i][j - 1].z;
-		coord[i][j].y = 50 + coord[i][j - 1].y;
-		++j;
+		++nbline;
 		++map;
 	}
+	return (nline);
 }
 
-void		fill_tab_coord(char ***map, t_coord ****coord)
+t_coord		***init_tab_coord(char ***map)
 {
-	fill_line_tab_coord(*map, **coord);
+	t_coord	***coord;
+	int		nline;
+	int		i;
+
+	nline = nbline(map);
+	coord = NULL;
+	if ((coord = (t_coord***)malloc(sizeof(t_coord**) * (nbline + 1))))
+	{
+		i = -1;
+		while (++i < nline)
+		{
+			if (i == 0)
+				coord[i] = init_line_coord(*map++, NULL);
+			else
+				coord[i] = init_line_coord(*map++, coord[i - 1]);
+		}
+		coord[nbline] = NULL;
+	}
+	return (coord);
 }
