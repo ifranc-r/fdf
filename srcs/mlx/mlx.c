d25972b712bf3f0eae66d9c3f18aa92b19da14dc
Andy 42 <aramanan@MacBook-Pro-de-Travaille.local>
2016-02-29 16:27:38 +0100

    ajout de mlx_pixel_put_image

--- srcs/mlx/mlx.c
+++ srcs/mlx/mlx.c
@@ -6,7 +6,7 @@
 /*   By: aramanan <aramanan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/16 20:40:41 by aramanan          #+#    #+#             */
-/*   Updated: 2016/02/27 16:07:57 by aramanan         ###   ########.fr       */
+/*   Updated: 2016/02/29 16:04:11 by aramanan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,27 @@
 
 void	init_mlx(t_all *all)
 {
-	//all->img.img = mlx_new_image(all->mlx, 1000, 1000);
 	all->mlx = mlx_init();
 	all->win = mlx_new_window(all->mlx, 1500, 1000, "FDF");
+	all->img = mlx_new_image(all->mlx, 1500, 1000);
+}
+
+void	mlx_pixel_put_image(void *img, int x, int y, int color)
+{
+	t_pic	p;
+	int		i;
+
+	p.data = mlx_get_data_addr(img, &(p.bpp), &(p.sizeline), &(p.endian));
+	i = x * 4 + y * p.sizeline;
+	if (x >= 0 && y >= 0)
+	{
+		p.data[i] = color % 256;
+		color /= 256;
+		p.data[i + 1] = color % 256;
+		color /= 256;
+		p.data[i + 2] = color % 256;
+		color /= 256;
+		p.data[i + 3] = 0;
+		color /= 256;
+	}
 }
