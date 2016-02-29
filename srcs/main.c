d266eb048519605b6eb23094ff4618f43ee44b2a
Inti FRANC-REGIS <ifranc-r@e1r9p12.42.fr>
2016-02-29 21:41:01 +0100

    inage

--- srcs/main.c
+++ srcs/main.c
@@ -15,17 +15,14 @@
 int		main(int argc, char **argv)
 {
 	t_all		all;
-	// int		fd;
 	char		***map;
-	int			i;
-//	int			j;
 
 	if (argc > 1)
 	{
 		map = content_read_file(argv[1]);
 		all.coord = init_tab_coord(map);
 		init_mlx(&all);
-		i = -1;
+		all.img.data = mlx_get_data_addr(all.img.img, &all.img.bitpx, &all.img.size, &all.img.endian);
 		ft_pixel_put(all);
 		mlx_key_hook(all.win, mlx_funct_key, all.mlx);
 		mlx_loop(all.mlx);
