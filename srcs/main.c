d3819384b10b5128ca7d62fa1e7900c5a9810365
Andy 42 <aramanan@MacBook-Pro-de-Travaille.local>
2016-02-29 18:28:16 +0100

    test dessin ok

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: aramanan <aramanan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/01 11:58:19 by ifranc-r          #+#    #+#             */
-/*   Updated: 2016/02/29 16:35:40 by aramanan         ###   ########.fr       */
+/*   Updated: 2016/02/29 18:27:43 by aramanan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ static	void	test_img(t_all all)
 			mlx_pixel_put_image(all.img, all.coord[i][j]->x, all.coord[i][j]->z, all.coord[i][j]->color);
 	}
 }
+
 int		main(int argc, char **argv)
 {
 	t_all		all;
@@ -43,23 +44,11 @@ int		main(int argc, char **argv)
 		all.coord = init_tab_coord(map);
 		init_mlx(&all);
 		// test
+		g() = all;
 		test_img(all);
 		mlx_put_image_to_window(all.mlx, all.win, all.img, 0, 0);
 		// fin test
 		//ft_pixel_put(all);
-/*		while (all.coord[++i])
-		{
-			j = -1;
-			while (all.coord[i][++j])
-			{
-				printf("valeur de x: %d\nvaleur de y: %d\nvaleur de z: %d\n\n ", all.coord[i][j]->x, all.coord[i][j]->y, all.coord[i][j]->z);
-				mlx_pixel_put(all.mlx, all.win, all.coord[i][j]->x, all.coord[i][j]->z, 0xFFFFFF);
-			}
-		}*/
-		// fd = open(argv[1], O_RDONLY);
-		// map = read_map(fd);
-		// init_mlx(&all);
-		// // ft_pixel_put(all.mlx, all.win, map);
 		mlx_key_hook(all.win, mlx_funct_key, all.mlx);
 		mlx_loop(all.mlx);
 	}
